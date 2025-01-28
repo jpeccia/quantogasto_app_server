3010fbffc519bef1d3af49db4ff6df0bd11d1f95
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-28 00:25:22 -0300

    feat: add database conect and load dot env in main file

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/jpeccia/quantogasto_app_server/database"
 	"github.com/jpeccia/quantogasto_app_server/handlers"
 )
 
 func main() {
+	// Carrega as variáveis de ambiente do arquivo .env
+	if err := godotenv.Load(); err != nil {
+        log.Fatal("Erro ao carregar o arquivo .env")
+    }
+
+    // Conecta ao banco de dados
+    if err := database.Connect(); err != nil {
+        log.Fatal("Erro ao conectar ao banco de dados: ", err)
+    }
+	
 	r := gin.Default()
 
 	r.POST("/renda", handlers.AdicionarRenda)
