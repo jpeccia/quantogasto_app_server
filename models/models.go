ea4ef7cf83c02e054ef0418cab8c68bb7eed0c99
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-28 00:43:24 -0300

    feat: add Usuario in models.go

--- models/models.go
+++ models/models.go
@@ -1,20 +1,25 @@
 package models
 
+import "time"
+
 type Usuario struct {
-	ID	int	`json:"id"`
-	Nome	string	`json:"nome"`
-	Renda	float64	`json:"renda"`
+	ID         int       `json:"id"`
+	Nome       string    `json:"nome"`
+	FotoPerfil string    `json:"foto_perfil"` // URL ou caminho da foto
+	Cargo      string    `json:"cargo"`
+	Renda      float64   `json:"renda"`
+	CreatedAt  time.Time `json:"created_at"`
 }
 
 type GastoFixo struct {
-	ID	int	`json:"id"`
-	Nome	string	`json:"nome"`
-	Valor	float64	`json:"valor"`
+	ID    int     `json:"id"`
+	Nome  string  `json:"nome"`
+	Valor float64 `json:"valor"`
 }
 
 type GastoVariavel struct {
-	ID	int	`json:"id"`
-	Nome	string	`json:"nome"`
-	Valor	float64	`json:"valor"`
-	Data	string	`json:"data"`
+	ID    int     `json:"id"`
+	Nome  string  `json:"nome"`
+	Valor float64 `json:"valor"`
+	Data  string  `json:"data"`
 }
